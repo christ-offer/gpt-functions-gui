1eec0160e9c2050ef120be502134916da81348dc
Chris Holst <chris@seam.es>
2023-06-17 23:36:25 +0200

    added in sparql query function for wikidata, its a bit janky atm, but somewhat works. Also a system message that is currently not in use for a code assistant

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import re
 import openai
 import json
 import sys
+import requests
 import io
 import logging
 import pandas as pd
 from contextlib import redirect_stdout
 from typing import Optional, Dict, List, Tuple
+
 
 # Define the directory where the knowledgebase files are stored
 KB_DIR = "./kb"
@@ -26,9 +28,42 @@ def ensure_directory_exists(directory: str):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def wikidata_sparql_query(query: str) -> str:
+    url = "https://query.wikidata.org/sparql"
+    headers = {
+        "Accept": "application/sparql-results+json",
+        "User-Agent": "SPARQL Query GPT"
+    }
+    params = {"query": query, "format": "json"}
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()  # Raises a HTTPError if the status is 4xx, 5xx
+
+        json_response = response.json()
+
+        head = json_response.get("head", {}).get("vars", [])
+        results = json_response.get("results", {}).get("bindings", [])
+
+        # Convert the 'head' and 'results' into strings and return them
+        result_str = 'Variables: ' + ', '.join(head) + '\n'
+        result_str += 'Results:\n'
+        for result in results:
+            for var in head:
+                result_str += f'{var}: {result.get(var, {}).get("value", "N/A")}\n'
+            result_str += '\n'
+        return result_str
+    except requests.HTTPError as e:
+        return f"A HTTP error occurred: {str(e)}"
+    except requests.RequestException as e:
+        return f"A request exception occurred: {str(e)}"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+
 def write_code_file(filename: str, content: str) -> str:
     if not is_valid_filename(filename):
-        raise ValueError(f"Invalid filename: {filename}")
+        return f"Invalid filename: {filename}"
     # directory = os.path.dirname(filename)
     # ensure_directory_exists(directory)
     try:
@@ -36,12 +71,11 @@ def write_code_file(filename: str, content: str) -> str:
             f.write(content)
         return f"File '{filename}' has been successfully written."
     except Exception as e:
-        logging.error(f"An error occurred while writing the file: {str(e)}")
-        raise
+        return f"An error occurred while writing the file: {str(e)}"
 
 def knowledgebase_create_entry(filename: str, content: str) -> str:
     if not is_valid_filename(filename):
-        raise ValueError("The provided filename is not valid.")
+        return "The provided filename is not valid."
     
     ensure_directory_exists(KB_DIR)
     filepath = os.path.join(KB_DIR, filename)
@@ -51,12 +85,11 @@ def knowledgebase_create_entry(filename: str, content: str) -> str:
             f.write(content)
         return f"Entry '{filename}' has been successfully created."
     except Exception as e:
-        logging.error(f"An error occurred while creating the entry: {str(e)}")
-        raise
+        return f"An error occurred while creating the entry: {str(e)}"
 
 def knowledgebase_read_entry(filename: str) -> str:
     if not is_valid_filename(filename):
-        raise ValueError("The provided filename is not valid.")
+        return "The provided filename is not valid."
     
     filepath = os.path.join(KB_DIR, filename)
     
@@ -66,25 +99,23 @@ def knowledgebase_read_entry(filename: str) -> str:
         html_content = markdown.markdown(content)
         return html_content
     except Exception as e:
-        logging.error(f"An error occurred while reading the entry: {str(e)}")
-        raise
+        return f"An error occurred while reading the entry: {str(e)}"
 
 def knowledgebase_update_entry(filename: str, content: str) -> str:
     if not is_valid_filename(filename):
-        raise ValueError("The provided filename is not valid.")
+        return "The provided filename is not valid."
     
     filepath = os.path.join(KB_DIR, filename)
     
     if not os.path.exists(filepath):
-        raise ValueError("The specified entry does not exist.")
+        return "The specified entry does not exist."
     
     try:
         with open(filepath, 'w') as f:
             f.write(content)
         return f"Entry '{filename}' has been successfully updated."
     except Exception as e:
-        logging.error(f"An error occurred while updating the entry: {str(e)}")
-        raise
+        return f"An error occurred while updating the entry: {str(e)}"
 
 def knowledgebase_list_entries() -> str:
     ensure_directory_exists(KB_DIR)
@@ -93,8 +124,7 @@ def knowledgebase_list_entries() -> str:
         entries_str = '\n'.join(entries)
         return f"The knowledgebase contains the following entries:\n{entries_str}"
     except Exception as e:
-        logging.error(f"An error occurred while listing the entries: {str(e)}")
-        raise
+        return f"An error occurred while listing the entries: {str(e)}"
 
 def python_repl(code: str) -> str:
     buffer = io.StringIO()
@@ -102,8 +132,7 @@ def python_repl(code: str) -> str:
         try:
             exec(code, {"__name__": "__main__"})
         except Exception as e:
-            logging.error(f"An error occurred while running the code: {str(e)}")
-            raise
+            return f"An error occurred while running the code: {str(e)}"
     return buffer.getvalue()
 
 def read_csv_columns(file_path: str) -> str:
@@ -112,11 +141,9 @@ def read_csv_columns(file_path: str) -> str:
         columns = df.columns.tolist()
         return ', '.join(columns) # Return the list as a string
     except FileNotFoundError:
-        logging.error(f"The file '{file_path}' does not exist.")
-        raise
+        return f"The file '{file_path}' does not exist."
     except Exception as e:
-        logging.error(f"An error occurred while reading the CSV file: {str(e)}")
-        raise
+        return f"An error occurred while reading the CSV file: {str(e)}"
 
 
 def run_conversation(prompt: str, conversation: List[Dict[str, str]]) -> Tuple[str, List[Dict[str, str]]]:
@@ -203,7 +230,18 @@ def run_conversation(prompt: str, conversation: List[Dict[str, str]]) -> Tuple[s
                     },
                     "required": ["filename", "content"],
                 },
-            }
+            },
+            {
+                "name": "wikidata_sparql_query",
+                "description": "Executes a SPARQL query on Wikidata and returns the result as a JSON string.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {"type": "string", "description": "The SPARQL query to execute. Must be a SINGLE LINE STRING!"},
+                    },
+                    "required": ["query"],
+                },
+            },
         ],
         function_call="auto",
     )
@@ -223,6 +261,8 @@ def run_conversation(prompt: str, conversation: List[Dict[str, str]]) -> Tuple[s
                                "knowledgebase_update_entry", "knowledgebase_list_entries", 
                                "read_csv_columns", "write_code_file"]:
             function_response = globals()[function_name](*function_args.values())
+        elif function_name == "wikidata_sparql_query":
+            function_response = wikidata_sparql_query(function_args.get("query"))
 
         logging.info(f"Function response: {function_response}")
         
