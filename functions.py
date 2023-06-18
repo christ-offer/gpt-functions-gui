8b4ccd4e78f8f3c8dc660cfbc5e99ff7e8326ed6
Chris Holst <chris@seam.es>
2023-06-18 20:54:25 +0200

    added in ability to image to text urls

--- functions.py
+++ functions.py
@@ -8,6 +8,8 @@ import pandas as pd
 import torch
 import open_clip
 from PIL import Image
+import tempfile
+import urllib
 
 from utils import is_valid_filename, ensure_directory_exists
 
@@ -76,7 +78,7 @@ def scrape_webpage(url: str) -> str:
     except Exception as e:
         return f"An error occurred: {e}"
 
-def image_to_text(image_path, seq_len=20):
+def image_to_text(image_path_or_url, seq_len=20):
     device = torch.device("cpu")
 
     try:
@@ -86,15 +88,26 @@ def image_to_text(image_path, seq_len=20):
         )
         model.to(device)
 
-        image = Image.open(image_path).convert("RGB")
+        # Check if the image_path_or_url is a URL or local path
+        if urllib.parse.urlparse(image_path_or_url).scheme in ['http', 'https']:
+            response = requests.get(image_path_or_url)
+            _, ext = os.path.splitext(urllib.parse.urlparse(image_path_or_url).path)
+            file_name = urllib.parse.urlparse(image_path_or_url).path.split('/')[-1]
+            with open(f'data/{file_name}', 'wb') as f:
+                f.write(response.content)
+            image = Image.open(f'data/{file_name}').convert("RGB")
+        else:
+            image = Image.open(image_path_or_url).convert("RGB")
+
         im = transform(image).unsqueeze(0).to(device)
-    
+
         with torch.no_grad(), torch.cuda.amp.autocast():
             generated = model.generate(im, seq_len=seq_len)
-        
+
         return open_clip.decode(generated[0].detach()).split("<end_of_text>")[0].replace("<start_of_text>", "")
+
     except FileNotFoundError:
-        return f"No file found at {image_path}"
+        return f"No file found at {image_path_or_url}"
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
