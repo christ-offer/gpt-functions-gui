e17fb564ff9c4fd94f72faf3a6eb7c707d244dd3
Chris Holst <chris@seam.es>
2023-06-20 09:04:43 +0200

    added tabs for history and knowledgebase, where you can select th efile from the sidebar and see it rendered as markdown/html, although pretty basic, in the main window

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import logging
-import tkinter as tk
-from tkinter import scrolledtext
-from ttkthemes import ThemedTk
-from tkinter import ttk
 import os
+import logging
+import requests
 import threading
-from tkinter import filedialog
 import shutil
+import markdown
+import tkinter as tk
+from tkinter import ttk, Listbox, Scrollbar, N, S, E, W, scrolledtext, messagebox, filedialog
+from ttkthemes import ThemedTk
+from tkhtmlview import HTMLLabel
 from PIL import Image, ImageTk
 
 from chatbot import run_conversation
-from functions import function_params, wikidata_sparql_query, scrape_webpage, write_file, knowledgebase_create_entry, knowledgebase_list_entries, knowledgebase_read_entry, python_repl, read_csv_columns, image_to_text, read_file, edit_file, list_history_entries, write_history_entry, read_history_entry, query_wolframalpha
 
-conversation = []
+#conversation = []
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
@@ -28,17 +28,83 @@ class ChatbotGUI:
         self.conversation = []  # Add this line
         self.style = ttk.Style()
         self.style.configure('TButton', font=('Ubuntu', 14))
-        self.create_widgets()
+        self.md_directory = "kb/"  # Set path to markdown files
+        self.history_directory = "history/"  # Set path to markdown files
+        
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=4)
+
+        # Create notebook (tab control)
+        self.tabControl = ttk.Notebook(self.root)
+        self.tabControl.grid(row=0, column=0, rowspan=2, columnspan=2, sticky='nsew')
+
+        # Create Tab 1
+        self.tab1 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab1, text='Chat')
+
+        # Create widgets in Tab 1
+        self.create_widgets_tab1()
+        
+        # Create Tab 2
+        self.tab2 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab2, text='KnowledgeBase')
+
+        # Create widgets in Tab 2
+        self.create_widgets_tab2()
         
         self.is_loading = False
+        
+        # Create Tab 3
+        self.tab3 = ttk.Frame(self.tabControl)
+        self.tabControl.add(self.tab3, text='History')
+        
+        # Create widgets in Tab 3
+        self.create_widgets_tab3()
+    
+    def create_widgets_tab1(self):
+        self.tab1.grid_rowconfigure(0, weight=1)  # Add this line
+        self.tab1.grid_columnconfigure(0, weight=1)  # Add this line
+        self.tab1.grid_columnconfigure(1, weight=0)  # Add this line
+        self.tab1.grid_columnconfigure(2, weight=4)  # Add this line
+        ttk.Label(self.tab1, text ="Chatbot").grid(column = 0, row = 0, padx = 30, pady = 30, sticky='nsew')  # Add this parameter
+        self.create_sidebar(self.tab1)
+        self.create_main_area(self.tab1)
 
-    def create_widgets(self):
-        self.create_sidebar()
-        self.create_main_area()
-        self.root.bind('<Return>', self.run_chat)  # If you press 'Enter', it will trigger the run_chat function
+        # bind 'Enter' to run_chat function in Tab 1 only
+        self.tab1.bind('<Return>', self.run_chat)
 
-    def create_sidebar(self):
-        self.sidebar = ttk.Frame(self.root, width=200)
+    def create_widgets_tab2(self):
+        self.tab2.grid_rowconfigure(0, weight=1)  # Add this line
+        self.tab2.grid_columnconfigure(0, weight=1)  # Add this line
+        self.tab2.grid_columnconfigure(1, weight=0)  # Add this line
+        self.tab2.grid_columnconfigure(2, weight=4)  # Add this line
+
+        ttk.Label(self.tab2, text ="KnowledgeBase").grid(column = 0, 
+                                                       row = 0, 
+                                                       padx = 30, 
+                                                       pady = 30, 
+                                                       sticky='nsew')  # Add this parameter
+        self.create_md_sidebar(self.tab2)
+        self.create_md_viewer(self.tab2)
+    
+    def create_widgets_tab3(self):
+        self.tab3.grid_rowconfigure(0, weight=1)
+        self.tab3.grid_columnconfigure(0, weight=1)
+        self.tab3.grid_columnconfigure(1, weight=0)
+        self.tab3.grid_columnconfigure(2, weight=4)
+        
+        ttk.Label(self.tab3, text ="History").grid(column = 0,
+                                                    row = 0,
+                                                    padx = 30,
+                                                    pady = 30,
+                                                    sticky='nsew')
+        self.create_history_sidebar(self.tab3)
+        self.create_history_viewer(self.tab3)
+
+
+    def create_sidebar(self, parent):
+        self.sidebar = ttk.Frame(parent, width=200)
         self.sidebar.grid(row=0, column=0, sticky='nsew')
 
         # Add a widget to make the sidebar visible. Adjust as necessary for your design.
@@ -68,6 +134,33 @@ class ChatbotGUI:
         theme_dropdown = ttk.Combobox(self.sidebar, textvariable=self.theme_var, values=available_themes)
         theme_dropdown.pack(side=tk.BOTTOM, anchor='s')
         theme_dropdown.bind('<<ComboboxSelected>>', self.change_theme)  # bind the selection event to the change_theme method
+    
+    def create_main_area(self, parent):
+        main_frame = ttk.Frame(parent)
+        main_frame.grid(row=0, column=2, sticky=N+S+E+W)
+        main_frame.grid_rowconfigure(0, weight=1)
+        main_frame.grid_columnconfigure(0, weight=1)
+
+        self.create_text_area(main_frame)
+        self.create_input_area(main_frame)
+
+    def create_text_area(self, frame):
+        self.text_area = scrolledtext.ScrolledText(frame, wrap='word', background="oldlace", font=('Ubuntu', 16))
+        self.text_area.tag_configure("user", background="lightgray", spacing1=30, spacing3=10)  # style for user text
+        self.text_area.tag_configure("bot", background="lightblue", spacing1=30, spacing3=10)  # style for bot text
+        self.text_area.grid(row=0, column=0, sticky='nsew')
+
+    def create_input_area(self, frame):
+        bottom_frame = ttk.Frame(frame)
+        bottom_frame.grid(sticky='nsew')
+
+        self.user_input_text = scrolledtext.ScrolledText(bottom_frame, wrap='word', background="oldlace", height=2, font=('Ubuntu', 16)) # Set the font size and height
+        self.user_input_text.grid(row=0, column=0, sticky='nsew')
+
+        send_button = ttk.Button(bottom_frame, text='Send', command=self.run_chat, style='TButton')
+        send_button.grid(row=0, column=1)
+
+        bottom_frame.grid_columnconfigure(0, weight=1)
 
     def open_settings(self):
         # Create a new settings window
@@ -147,35 +240,6 @@ class ChatbotGUI:
                 "role": "assistant",
                 "content": f'Bot: {os.path.basename(csv_file_path)} was added to CSV folder\nThe columns are: {columns}\n',  # directly add function response to the conversation
             })
-
-
-
-    def create_main_area(self):
-        main_frame = ttk.Frame(self.root)
-        main_frame.grid(row=0, column=1, sticky='nsew')
-        main_frame.grid_rowconfigure(0, weight=1)
-        main_frame.grid_columnconfigure(0, weight=1)
-
-        self.create_text_area(main_frame)
-        self.create_input_area(main_frame)
-
-    def create_text_area(self, frame):
-        self.text_area = scrolledtext.ScrolledText(frame, wrap='word', background="oldlace", font=('Ubuntu', 16))
-        self.text_area.tag_configure("user", background="lightgray", spacing1=30, spacing3=10)  # style for user text
-        self.text_area.tag_configure("bot", background="lightblue", spacing1=30, spacing3=10)  # style for bot text
-        self.text_area.grid(row=0, column=0, sticky='nsew')
-
-    def create_input_area(self, frame):
-        bottom_frame = ttk.Frame(frame)
-        bottom_frame.grid(sticky='nsew')
-
-        self.user_input_text = scrolledtext.ScrolledText(bottom_frame, wrap='word', background="oldlace", height=2, font=('Ubuntu', 16)) # Set the font size and height
-        self.user_input_text.grid(row=0, column=0, sticky='nsew')
-
-        send_button = ttk.Button(bottom_frame, text='Send', command=self.run_chat, style='TButton')
-        send_button.grid(row=0, column=1)
-
-        bottom_frame.grid_columnconfigure(0, weight=1)
     
     def change_theme(self, event):
         selected_theme = self.theme_var.get()
@@ -197,9 +261,11 @@ class ChatbotGUI:
     def handle_model_response(self, user_input):
         try:
             response, _ = run_conversation(user_input, self.conversation)
-        except Exception as e:
-            response = f"Oops! An server error occurred, please try again."
+        except requests.exceptions.RequestException as e:  
+            response = "Oops! A network error occurred, please try again later."
             print(e)
+        except Exception as e:
+            response = f"Oops! An error occurred: {str(e)}"
 
         # Update the text area with the response
         # Because we are updating the UI from a different thread, we must use the 'after' method
@@ -212,12 +278,117 @@ class ChatbotGUI:
 
         self.text_area.insert(tk.INSERT, f'Bot: {response}\n', "bot")
 
-    
-
-
     def reset_conversation(self):
         self.conversation = []
         self.text_area.delete('1.0', tk.END)
+        
+    # TAB 2
+    
+    def create_md_sidebar(self, parent):
+        self.md_listbox = Listbox(parent)
+        self.md_listbox.grid(row=0, 
+                             column=0, 
+                             sticky=N+S+E+W)  # Add these parameters
+
+        scrollbar = Scrollbar(parent, orient="vertical", command=self.md_listbox.yview)
+        scrollbar.grid(row=0, 
+                       column=1, 
+                       sticky=N+S)  
+        self.md_listbox.config(yscrollcommand=scrollbar.set)
+
+        # Load the list of markdown files
+        self.update_md_files()
+
+        # Add event handler for selecting an item
+        self.md_listbox.bind('<<ListboxSelect>>', self.on_md_file_select)
+
+    def create_md_viewer(self, parent):
+        self.md_viewer = HTMLLabel(parent, html="<h1>Select a file from the list</h1>")
+        self.md_viewer.grid(row=0, 
+                            column=2, 
+                            sticky=N+S+E+W)  # Add these parameters
+
+    def update_md_files(self):
+        # Empty the list
+        self.md_listbox.delete(0, 'end')
+
+        # Get list of files in directory
+        files = os.listdir(self.md_directory)
+        md_files = [file for file in files if file.endswith('.md')]
+
+        # Add files to listbox
+        for file in md_files:
+            self.md_listbox.insert('end', file)
+
+    def on_md_file_select(self, evt):
+        # Get selected file name
+        idxs = self.md_listbox.curselection()
+        if len(idxs)==1:
+            idx = int(idxs[0])
+            md_file = self.md_listbox.get(idx)
+            
+            # Read and convert the markdown file to HTML
+            with open(os.path.join(self.md_directory, md_file), 'r') as f:
+                md_text = f.read()
+            html = markdown.markdown(md_text)
+            
+            # Display HTML in the viewer
+            self.md_viewer.set_html(html)
+    
+    # TAB 3
+    def create_history_sidebar(self, parent):
+        self.history_listbox = Listbox(parent)
+        self.history_listbox.grid(row=0, 
+                             column=0, 
+                             sticky=N+S+E+W)
+        
+        scrollbar = Scrollbar(parent, orient="vertical", command=self.history_listbox.yview)
+        scrollbar.grid(row=0,
+                          column=1,
+                          sticky=N+S)
+        self.history_listbox.config(yscrollcommand=scrollbar.set)
+        
+        # Load the list of markdown files
+        self.update_history_files()
+        
+        # Add event handler for selecting an item
+        
+        self.history_listbox.bind('<<ListboxSelect>>', self.on_history_file_select)
+        
+    def create_history_viewer(self, parent):
+        self.history_viewer = HTMLLabel(parent, html="<h1>Select a file from the list</h1>")
+        self.history_viewer.grid(row=0,
+                                 column=2,
+                                 sticky=N+S+E+W)
+        
+    def update_history_files(self):
+        # Empty the list
+        self.history_listbox.delete(0, 'end')
+        
+        # Get list of files in directory
+        files = os.listdir(self.history_directory)
+        history_files = [file for file in files if file.endswith('.md')]
+        
+        # Add files to listbox
+        for file in history_files:
+            self.history_listbox.insert('end', file)
+            
+    def on_history_file_select(self, evt):
+        # Get selected file name
+        idxs = self.history_listbox.curselection()
+        if len(idxs)==1:
+            idx = int(idxs[0])
+            history_file = self.history_listbox.get(idx)
+            
+            # Read and convert the markdown file to HTML
+            with open(os.path.join(self.history_directory, history_file), 'r') as f:
+                history_text = f.read()
+            html = markdown.markdown(history_text)
+            
+            # Display HTML in the viewer
+            self.history_viewer.set_html(html)
+            
+            
 
     def run(self):
         self.root.mainloop()
